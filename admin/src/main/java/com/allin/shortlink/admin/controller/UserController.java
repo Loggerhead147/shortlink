98d4b5eb34685bb38e08ba95b75945217d0c169d
Allin <allin147@163.com>
2024-05-30 22:29:28 +0800

    feature: 用户敏感信息脱敏功能模块

--- admin/src/main/java/com/allin/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/allin/shortlink/admin/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.allin.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.allin.shortlink.admin.common.convention.result.Result;
 import com.allin.shortlink.admin.common.convention.result.Results;
+import com.allin.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.allin.shortlink.admin.dto.resp.UserRespDTO;
 import com.allin.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,16 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-
     private final UserService userService;
 
     /**
-     *  根据用户名查询用户信息
+     *  根据用户名查询用户信息（脱敏）
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
-        UserRespDTO res = userService.getUserByUsername(username);
-        return Results.success(res);
+        return Results.success(userService.getUserByUsername(username));
     }
 
+    /**
+     *  根据用户名查询用户信息（真实）
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username){
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username),UserActualRespDTO.class));
+    }
 }
--- admin/src/main/java/com/allin/shortlink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/allin/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -1,14 +1,12 @@
 package com.allin.shortlink.admin.dto.resp;
 
-import com.allin.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 
 /**
  * 用户返回参数响应
  */
 @Data
-public class UserRespDTO {
+public class UserActualRespDTO {
 
     /**
      * id
@@ -28,7 +26,6 @@ public class UserRespDTO {
     /**
      * 手机号
      */
-    @JsonSerialize(using= PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
