372caa679ed07ffd3a5712c68af0f513db01d35b
Allin <allin147@163.com>
2024-05-31 16:08:23 +0800

    feature: 开发短链接后管系统用户注册相关功能

--- admin/src/main/java/com/allin/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/allin/shortlink/admin/service/UserService.java
@@ -1,6 +1,7 @@
 package com.allin.shortlink.admin.service;
 
 import com.allin.shortlink.admin.dao.entity.UserDO;
+import com.allin.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.allin.shortlink.admin.dto.resp.UserRespDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -15,4 +16,17 @@ public interface UserService extends IService<UserDO> {
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 判断用户名是否存在
+     * @param username 用户名
+     * @return 用户存在返回 True，不存在返回 False
+     */
+    Boolean hasUsername(String username);
+
+    /**
+     * 注册用户
+     * @param requestParam 注册用户请求参数
+     */
+    void register(UserRegisterReqDTO requestParam);
 }
