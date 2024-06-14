ee0c55f183c178e4b87796c488eada4444a44efb
Loggerhead147 <allin147@163.com>
2024-06-14 16:31:59 +0800

    feature: 短链接后管系统接口层代码可读性优化

--- admin/src/main/java/com/allin/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/allin/shortlink/admin/service/GroupService.java
@@ -16,26 +16,31 @@ public interface GroupService extends IService<GroupDO> {
 
     /**
      * 新增短链接分组
+     * @param groupName 短链接分组名称
      */
     void saveGroup(String groupName);
 
     /**
      * 查询用户短链接分组集合
+     * @return 用户短链接分组集合
      */
     List<ShortLinkGroupRespDTO> listGroup();
 
     /**
      * 修改用户短链接分组名称
+     * @param requestParam 短链接分组名称
      */
     void updateGroup(ShortLinkGroupUpdateReqDTO requestParam);
 
     /**
      * 删除用户短链接分组
+     * @param gid 分组标识
      */
     void deleteGroup(String gid);
 
     /**
      * 短链接分组排序
+     * @param requestParam 短链接分组集合
      */
     void sortGroup(List<ShortLinkGroupSortReqDTO> requestParam);
 }
